eb77bfa7ac7cb9386dad3e86000aa2c844b48639
Arturo Sáenz <saenzsanchezblack@hotmail.com>
2025-11-26 17:52:41 -0600

    Update App.tsx

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -6,6 +6,7 @@ import { NosotrosPage } from "./modules/nosotros/pages/NosotrosPage";
 import { EventosPage } from "./modules/eventos/pages/EventosPage";
 import { NoticiasPage } from "./modules/noticias/pages/NoticiasPage";
 import { CerrosPage } from "./modules/cerros/pages/CerrosPage";
+import { CerroDetallePage } from "./modules/cerros/pages/CerroDetallePage";
 import { MapaPage } from "./modules/mapa/pages/MapaPage";
 import { GaleriaPage } from "./modules/galeria/pages/GaleriaPage";
 import { FAQPage } from "./modules/faq/pages/FAQPage";
@@ -23,6 +24,7 @@ function App() {
         <Route path={ROUTES.EVENTOS} component={EventosPage} />
         <Route path={ROUTES.NOTICIAS} component={NoticiasPage} />
         <Route path={ROUTES.CERROS} component={CerrosPage} />
+        <Route path={ROUTES.CERRO_DETALLE} component={CerroDetallePage} />
         <Route path={ROUTES.MAPA} component={MapaPage} />
         <Route path={ROUTES.GALERIA} component={GaleriaPage} />
         <Route path={ROUTES.VOLUNTARIADO} component={VolunteerPage} />
